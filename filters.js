1d83247a9a6994cdf82022c4e90b5967c299dc69
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-02-06 16:02:58 +0100

    add separate css file, replace buttons with range sliders

--- filters.js
+++ filters.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
     let plotno = document.querySelector('#plotno')
+
     let negatyw = document.querySelector('#negatyw')
-    let kontrastV = document.querySelector("#kontrast_wartosc")
     let black = document.querySelector("#black")
-    let jasnosc_minus = document.querySelector("#jasnosc_zmniejsz")
-    let jasnosc_plus = document.querySelector('#jasnosc_zwieksz')
+
+    let bright = document.querySelector("#bright")
+    let bright_counter = document.querySelector("#bright-counter")
+
+
     
+    console.log(negatyw)
     const zdjecie = './img.jpeg'
     let ctx = plotno.getContext('2d')
     let img = new Image()
@@ -14,31 +18,37 @@ document.addEventListener('DOMContentLoaded', function(){
         ctx.drawImage(img, 0, 0, plotno.width, plotno.height) 
     })
     
+    function redraw(){
+        ctx.drawImage(img, 0, 0, plotno.width, plotno.height)
+    }
     
     //JASNOSC
-    jasnosc_minus.addEventListener("click", (e)=>{
+    bright.addEventListener("input", (e)=>{
+        redraw()
         let imageData = ctx.getImageData(0, 0, plotno.width, plotno.height)
+        let value = (e.target.value)/10
+        bright_counter.innerHTML = value
+        console.log(value)
         for(let i = 0; i< imageData.data.length; i+=4){
-                imageData.data[i]   = imageData.data[i] / 1.3
-                imageData.data[i+1] = imageData.data[i+1] / 1.3
-                imageData.data[i+2] = imageData.data[i+2] / 1.3
+                imageData.data[i]   *= value
+                imageData.data[i+1] *= value
+                imageData.data[i+2] *= value
         }
+
         ctx.putImageData(imageData, 0, 0)
     })
-    jasnosc_plus.addEventListener("click", (e)=>{
-        let imageData = ctx.getImageData(0, 0, plotno.width, plotno.height)
-        for(let i = 0; i< imageData.data.length; i+=4){
-                imageData.data[i]   *= 1.3
-                imageData.data[i+1] *= 1.3
-                imageData.data[i+2] *= 1.3
-        }
-        ctx.putImageData(imageData, 0, 0)
-    })
+    
+    
+
+
+                            //////////////FILTRY
+
+
 
     //NEGATYW
     negatyw.addEventListener("click", (e)=>{
         let imageData = ctx.getImageData(0, 0, plotno.width, plotno.height)
-            
+        console.log(imageData)
         for(let i = 0; i< imageData.data.length; i+=4){
                 imageData.data[i]   = imageData.data[i]^255
                 imageData.data[i+1] = imageData.data[i+1]^255
@@ -51,11 +61,11 @@ document.addEventListener('DOMContentLoaded', function(){
         let imageData = ctx.getImageData(0, 0, plotno.width, plotno.height)
         for (let i = 0; i < imageData.data.length; i+= 4) {
         let avg = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3
-        imageData.data[i]     = avg
-        imageData.data[i + 1] = avg
-        imageData.data[i + 2] = avg
+            imageData.data[i]     = avg
+            imageData.data[i + 1] = avg
+            imageData.data[i + 2] = avg
         }
         ctx.putImageData(imageData, 0, 0);
-        })
     })
+})
 
